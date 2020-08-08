7060553fa14b3659f4036a9608c5fdfd0d7db610
Sya Raihan Heggi <58820833+RaihanHeggi@users.noreply.github.com>
2020-08-08 16:47:26 +0700

    2nd Program
    in this program's you can convert MPH to MPS

---
+++ Second Challenge (Miles Per Hour Conversion App)/Miles Per Hour Conversion App.py
@@ -0,0 +1,4 @@
+print("Welcome to the MPH and MPS Conversion App\n")
+milesPerHour = float(input('What is your speed in miles per hour: '))
+conversionToMPS = milesPerHour * 0.4474
+print("Your speed in meter per second is "+str(round(conversionToMPS, 2)))
